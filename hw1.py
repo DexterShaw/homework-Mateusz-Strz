f9bfa372985755e89360918126def186f69bf277
Mateusz-Strz <62249490+Mateusz-Strz@users.noreply.github.com>
2020-03-17 23:00:26 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -75,8 +75,6 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     :param year: Month to get the countries for as an integer indexed from 1
     :return: Number of countries/regions where the count has not changed in a day
     """
-    if day<=0 or month<=0 or year<=0:
-        raise ValueError("if either number was negative")
     today = datetime.date(year, month, day)
     yesterday = today + datetime.timedelta(days=-1)
     today = date.strftime('%-m/%-d/%y')
